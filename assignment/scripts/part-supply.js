fe586ced43e06a2d5a787c142f4e9d326b10b4f9
Abram Chavez <abram.chavez820@gmail.com>
2021-05-21 15:41:27 -0500

    console log index 2

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -13,7 +13,7 @@ let supplyChanges = ['3','5','-6','0','7','11'];
 console.log('2. Array of supplyChanges:');
 
 // 3. Console log the value of 'supplyChanges' at index 2
-console.log('3. Item at index 2 is:');
+console.log('3. Item at index 2 is:', supplyChanges[2]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
