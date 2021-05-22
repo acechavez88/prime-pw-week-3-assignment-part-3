464eccd1837e091100635ea5afe3600bb6c46bee
Abram Chavez <abram.chavez820@gmail.com>
2021-05-21 23:12:44 -0500

    attempt at 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -24,14 +24,19 @@ console.log('4. Removed item:', removedsupplyChanges);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 supplyChanges.push('25');
 console.log('5. Adding 25 to supplyChanges.');
+// added item  25 to array supplyChanges at the end. Confirmed in console.
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, skip it. Do not log it to the console.
 //    - if the value is negative, format the log as 'Part count -x.'
-console.log('6. Showing supplyChanges...');
 
+console.log('6. Showing supplyChanges...', supplyChanges);
+let x = supplyChanges;
+for (let i = 0; i < supplyChanges.length; i++) {
+  console.log('added 20 parts'[i]);
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
